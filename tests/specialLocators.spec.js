291b23dc3f6c8173e64fa907a44e18c4cf99b77b
Salma <salma.shaikh751@gmail.com>
2026-01-08 20:01:40 +0100

    added getByTEXT,getByPlaceholder,filter

--- tests/specialLocators.spec.js
+++ tests/specialLocators.spec.js
@@ -6,5 +6,11 @@ test('Playwright Special Locators',async ({page})=>{
     await page.getByLabel('Employed').check();
     await page.getByLabel('Gender').selectOption('Female');
     await page.getByPlaceholder('Password').fill('abc123');
+    await page.getByRole("button",{name:'Submit'}).click();
+    await page.getByText("Success!The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link",{name:"Shop"}).click();
+    await page.locator("app-card").filter({hasText:"Nokia Edge"}).getByRole("button").click()//here no need to filter button with name,as there is only one button
     
+
+
 })
